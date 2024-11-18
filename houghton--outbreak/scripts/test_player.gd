401be5a6563b7ce1c06745a9a1d2e2c426677a9e
AJordan2207 <alecjhungerford@gmail.com>
2024-11-18 18:08:12 -0500

    Revert "added sensitivity and cleaned movement code"
    This reverts commit e0246f4892a33424481957adafe6cc86737d18f8.

--- houghton--outbreak/scripts/test_player.gd
+++ houghton--outbreak/scripts/test_player.gd
@@ -7,7 +7,6 @@ const FORWARD_SPEED = 7.5
 const BACKWARD_SPEED = 5
 const TURNING_SPEED = 0.035
 const GRAVITY_CONSTANT = 100
-@export var turning_sensitivity: float = 1.0
 
 var input = Vector3.ZERO
 
@@ -20,33 +19,44 @@ func _physics_process(delta: float) -> void:
 	character_movement(delta)
 
 func character_movement(delta: float):
-	# Apply gravity to y-component of velocity
-	velocity.y -= delta * GRAVITY_CONSTANT
+	if Input.is_action_pressed("move_forwards") and Input.is_action_pressed("move_backwards"):
+		velocity.x = 0
+		velocity.z = 0
+		velocity.y -= delta*GRAVITY_CONSTANT
+
+	elif Input.is_action_pressed("move_forwards"):
+		var forwardVector = -Vector3.FORWARD.rotated(Vector3.UP, rotation.y)
+		velocity = -forwardVector * FORWARD_SPEED
+		velocity.y -= delta*GRAVITY_CONSTANT
+		
+	elif Input.is_action_pressed("move_backwards"):
+		var backwardVector = Vector3.FORWARD.rotated(Vector3.UP, rotation.y)
+		velocity = -backwardVector * BACKWARD_SPEED
+		velocity.y -= delta*GRAVITY_CONSTANT
 	
-	# If the forward input is pressed and the backwards one isn't
-	if Input.is_action_pressed("move_forwards") and !Input.is_action_pressed("move_backwards"):
-		var forwardVector = Vector3.FORWARD.rotated(Vector3.UP, rotation.y)
-		velocity.x = forwardVector.x * FORWARD_SPEED
-		velocity.z = forwardVector.z * FORWARD_SPEED
-	
-	#If the forward input is pressed and the backwards one isn't
-	elif Input.is_action_pressed("move_backwards") and !Input.is_action_pressed("move_forwards"):
-		var backwardVector = -Vector3.FORWARD.rotated(Vector3.UP, rotation.y)
-		velocity.x = backwardVector.x * BACKWARD_SPEED
-		velocity.z = backwardVector.z * BACKWARD_SPEED
-	
-	#If niether or both inputs are hit, set velocity to zero
 	else:
 		velocity.x = 0
 		velocity.z = 0
+		velocity.y -= delta*GRAVITY_CONSTANT
 	
-	#Handles turning
-	if Input.is_action_pressed("turn_left"):
-		rotation.y += input.y + TURNING_SPEED * turning_sensitivity
+	# If moving back while turning left, turn right
+	if Input.is_action_pressed("turn_left") and Input.is_action_pressed("move_backwards"):
+		rotation.y -= input.y + TURNING_SPEED 
+		velocity.y -= delta*GRAVITY_CONSTANT
+	
+	elif Input.is_action_pressed("turn_left"):
+		rotation.y += input.y + TURNING_SPEED 
+		velocity.y -= delta*GRAVITY_CONSTANT
+
+	# If moving back while turning right, turn left
+	if Input.is_action_pressed("turn_right") and Input.is_action_pressed("move_backwards"):
+		rotation.y += input.y + TURNING_SPEED 
+		velocity.y -= delta*GRAVITY_CONSTANT
 		
-	if Input.is_action_pressed("turn_right"):
-		rotation.y -= input.y + TURNING_SPEED * turning_sensitivity
-		
+	elif Input.is_action_pressed("turn_right"):
+		rotation.y -= input.y + TURNING_SPEED 
+		velocity.y -= delta*GRAVITY_CONSTANT
+	
 	move_and_slide()
 
 func hit():
