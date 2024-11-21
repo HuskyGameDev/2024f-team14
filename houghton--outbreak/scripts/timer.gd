18a216dfdaff706a3176a1551c902ce587474c12
LightningBlu <zanelong1@gmail.com>
2024-11-20 22:38:23 -0500

    Fixed Timer

--- houghton--outbreak/scripts/timer.gd
+++ houghton--outbreak/scripts/timer.gd
@@ -9,7 +9,7 @@ func _process(delta):
 			
 			var ms = fmod(time, 1)*1000 # 1000th of a second
 			var s = fmod(time, 60) # 60 sec in a min
-			var m = fmod(time, 60*60) # 60 sec in a min, 60 sec in an hour
+			var m = time / 60 # 60 sec in a min, 60 sec in an hour
 			
 			var timer = "%02d : %02d : %03d" % [m, s, ms]
 			text = timer
