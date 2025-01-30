c4ef0853c7e9bef2561311ecf17d4602dc18369c
LightningBlu <zanelong1@gmail.com>
2025-01-29 19:27:53 -0500

    Commit

--- houghton--outbreak/scripts/test_enemy.gd
+++ houghton--outbreak/scripts/test_enemy.gd
@@ -9,7 +9,7 @@ var health: int
 var player = null
 
 const FORWARD_SPEED = 3
-const ATTACK_RANGE = 2
+const ATTACK_RANGE = 5
 
 @export var player_path : NodePath
 
@@ -27,15 +27,19 @@ func _ready():
 
 func _process(delta):
 	velocity = Vector3.ZERO
-	
 	#navigation
 	nav_agent.set_target_position(player.global_transform.origin)
 	var next_nav_point = nav_agent.get_next_path_position()
-	velocity = (next_nav_point - global_transform.origin).normalized() * FORWARD_SPEED
+	#velocity = (next_nav_point - global_transform.origin).normalized() * FORWARD_SPEED
 	look_at(Vector3(player.global_position.x, global_position.y, player.global_position.z), Vector3.UP)
 	
 	#conditions
-	animtree.set("parameters/conditions/attack", _target_in_range())
+	#animtree.set("parameters/conditions/attack", _target_in_range())
+	if _target_in_range():
+		states.travel("attack")
+		await(get_tree().create_timer(1.6667))
+	else:
+		states.travel("walk")
 	move_and_slide()
 	death()
 
